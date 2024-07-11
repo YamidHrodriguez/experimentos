b5922b809ea87334aaa372f5058e0458d05135a1
practicante.ti <practicante.ti@egakat.com>
2024-07-11 19:24:15 +0000

    Volvi a la version anterior del codigo por errores

--- python/silogtran/backend/web2.py
+++ python/silogtran/backend/web2.py
@@ -1,45 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
+from itertools import cycle, islice
 import webbrowser
-from data.urls import urls
-from config.url_base import url_base
 
-urls = urls()
-url_base= url_base()
+url_base = "https://egakat.colombiasoftware.net/index.php?"
 
-def modify_addressee(manifiest,urls):
-    url = urls["masters"]["sender"]["change"]
-    webbrowser.open(url)
-    input()
-    url2= urls["ship"]["manifiest"]["DeleteCacheMin"]
-    webbrowser.open(url2)
-    input()
-    search_manifiest(manifiest,"ctl0_MainModule_EnviarMinisterio")
+# URL que quieres abrir
+urls =  {
+    "home":f"{url_base}#no-back-button",
+    "ship":{
+        "shipment":{
+            "form": f"{url_base}page=Despacho.Remesa_003.Home_010#no-back-button",
+            "search": f"{url_base}page=Despacho.Remesa_003.Home_010&remesa_codigo=",
+            },
+        "manifiest":f"{url_base}page=Despacho.Manifiesto_003.Home_010&"
+    },
+    "basics": {
+        "users":{
+            "create":f"{url_base}page=Basicos.Usuario.Home#no-back-button"
+        }
+    },
+    "masters":{
+        "sender":{
+            "change":"{url_base}page=Maestros.Remitente.CambioInformacion#no-back-button"
+        }
+    }
+}
 
-
-def open_shipment(type, urls, code,manifiest):
-    url = urls["ship"]["shipment"]["search"]
-    i = int(input("¿Cuántas remesas consecutivas quiere abrir? "))
-    for j in range(i):
-        webbrowser.open(f"{url}0{code+j}#no-back-button")
-    if type == "c":
-        modify_addressee(manifiest,urls)
-        
-def search_manifiest(manifiest,id):
-    webbrowser.open(f"{urls['ship']['manifiest']['form']}manifiesto_codigo=0{manifiest}#{id}")
+# def print_urls(d, indent=0):
+#     for key, value in d.items():
+#         if isinstance(value, dict):
+#             print(' ' * indent + f"{key}: ")
+#             print_urls(value, indent + 4)
+#         else:
+#             print(' ' * indent + f"{key}: {value}")
+def open_shipment(type,url,code):
+    url= urls["ship"]["shipment"]["search"]
+    i = int(input("Cúantas remesas consecutivas quiere abrir: "))
+    for i in range(i):
+        webbrowser.open(f"{url}0{code+i}#no-back-button")
+    if type=="c":
+        url = urls["masters"]["sender"]["change"]
+        webbrowser.open(url)
+    else:
+        pass
+    
 
 def error(type):
-    if type == 1:
+    if type==1:
         print("Error, digita una opción que se encuentre en el menú")
 
+
 def init():
-    question = input("""
-=============================================================================================================    
-Bienvenid@, ¿Iniciaste sesión ya?\nA) SÍ\nB) NO\nOpción: """).lower()
+    question = input("Bienvenid@, ¿Iniciaste sesión ya?\nA)SÍ\nB)NO\nOpcion: ").lower()
 
     if question == "b":
         webbrowser.open(url_base)
-        input()
     elif question == "a":
         pass
     else:
@@ -48,7 +64,7 @@ Bienvenid@, ¿Iniciaste sesión ya?\nA) SÍ\nB) NO\nOpción: """).lower()
     while True:
         print("""
 ==================================================
-Elije por favor una opción:
+Elije por favor una opcion:
     A) CREAR USUARIO
     B) MODIFICAR ORIGEN DE REMESAS
     C) MODIFICAR DESTINATARIO
@@ -60,30 +76,32 @@ Elije por favor una opción:
 
 
 def choose():
-    option = input("Opción: ").lower()
+
+    option = input("Opcion: ").lower()
 
     if option == "a":
         print("""
 ***************************************************************************
-                        CREACIÓN DE USUARIO
+                        CREACION DE USUARIO
 Abriendo formulario para creación de usuarios ....""")
-        webbrowser.open(urls["basics"]["users"]["create"])
+        webbrowser.open( urls["basics"]["users"]["create"])    
     elif option == "b":
         code_init = int(input("""
 ***************************************************************************
                     MODIFICAR ORIGEN DE REMESAS
 
 Digite el primer número de la lista de remesas a modificar: """))
-        open_shipment("b", urls, code_init,0)
+        open_shipment("b",urls,code_init)
     elif option == "c":
         print("""
 ***************************************************************************
                  MODIFICAR DESTINATARIO DE REMESAS
+        
         """)
         manifiest = int(input("Digita el número del manifiesto: "))
-        search_manifiest(manifiest,"ctl0_MainModule_Remesa")
-        code_init = int(input("Digite el primer número de la lista de remesas a modificar destinatario: "))
-        open_shipment("c", urls, code_init,manifiest)
+        webbrowser.open(f"{urls['ship']['manifiest']}manifiesto_codigo=0{manifiest}#no-back-button")
+        code_init = int(input("""Digite el primer número de la lista de remesas a modificar destinatario: """))
+        open_shipment("c",urls,code_init)
     elif option == "d":
         webbrowser.open(options["d"])
     elif option == "e":
@@ -93,5 +111,35 @@ Digite el primer número de la lista de remesas a modificar: """))
         print("Opción no válida. Inténtalo de nuevo.")
         return False
 
-#ALGORITMO PRINCIPAL
+
 init()
+
+
+# print_urls(urls)
+
+# Número de ciclos que deseas recorrer
+# num_cycles = 1
+
+# # Crea un iterador cíclico sobre los valores del diccionario
+# values_cycle = cycle(urls.values())
+
+# # Calcula el número total de iteraciones
+# total_iterations = num_cycles * len(urls)
+
+# # Recorre los valores de manera cíclica el número especificado de veces
+# for index, value in enumerate(islice(values_cycle, total_iterations), start=1):
+#     webbrowser.open(value)
+#     response = requests.get(value)
+#     if response.status_code == 200:
+#         # Parsear el HTML
+#         soup = BeautifulSoup(response.content, 'html.parser')
+#         html_content = soup.prettify()
+        
+#         # Guardar el HTML en un archivo
+#         file_name = f"page_{index}.html"
+#         with open(file_name, "w", encoding="utf-8") as file:
+#             file.write(html_content)
+        
+#         print(f"Guardado HTML de {value} en {file_name}")
+#     else:
+#         print(f"Error al acceder a {value}: {response.status_code}")
